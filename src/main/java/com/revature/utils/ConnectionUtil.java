5e267b0c4f055a90906312622999f9fb7d6d08d0
RoyMamba0329 <roy.mamba0329@gmail.com>
2019-09-26 21:10:55 -0400

    another commit for some fixing

--- src/main/java/com/revature/utils/ConnectionUtil.java
+++ src/main/java/com/revature/utils/ConnectionUtil.java
@@ -11,22 +11,13 @@ public class ConnectionUtil {
 	private static Connection conn = null;
 
 	public static Connection getConnection() {
-<<<<<<< HEAD
-		
-		try {
-			Class.forName("org.postgresql.driver");
-		} catch (ClassNotFoundException e1) {
-			e1.printStackTrace();
-		}
 
-=======
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
->>>>>>> f2ebc0c45b220120d13081f4c7dd88e7a842f5ae
 		try {
 			Properties props = new Properties();
 
